02fe5a2fb79e99305aed5d0b929c89f376f8d619
MAC <uzochuwubenamara@gmail.com>
2021-10-15 17:15:41 +0100

    Building the forms for create client

--- src/screens/client/CreateClient.tsx
+++ src/screens/client/CreateClient.tsx
@@ -3,7 +3,7 @@ import ClientListLayout from '../../Layout/ClientListLayout';
 import './pageStyles/createClient.scss';
 import ReusableButton from '../../components/button/ReusableButton';
 import { History } from 'history';
-import { Form, Col } from 'react-bootstrap';
+import { Form, Col, Row } from 'react-bootstrap';
 interface IHistory {
   history: History;
 }
@@ -11,6 +11,10 @@ interface IHistory {
 const CreateClient = ({ history }: IHistory) => {
   const createClient = () => {
     return <h2>Full Name</h2>;
+  };
+
+  const handleSubmit = () => {
+    console.log('Clicked');
   };
 
   return (
@@ -42,14 +46,65 @@ const CreateClient = ({ history }: IHistory) => {
           </Col>{' '}
           <Col lg={3} className="d-flex btn-left-wrapper">
             <div className="btn-left">
-              <ReusableButton
-                onClick={() => history.push('/create-client')}
-                text="Create Client"
-              />
+              <ReusableButton onClick={handleSubmit} text="Create Client" />
             </div>
           </Col>
         </div>
-        <div>REUSABLE TABLE AREA</div>
+        <hr className="line" />
+
+        <Row className="formArea">
+          <Col lg={6}>
+            <div className="formArea__inner__first">
+              <div className="formGroupMargin">
+                <p>Name &#x2A;</p>
+                <input
+                  className="formArea__inner__first__input"
+                  type="text"
+                />{' '}
+                <p className="text-area-input">
+                  This name would only be used to identify the client in the
+                  console and would not be shown to the end users.
+                </p>
+              </div>
+
+              <div className="formGroupMargin">
+                <p>Email (optional)</p>
+                <input
+                  dir="ltr"
+                  className="formArea__inner__first__input"
+                  type="email"
+                  placeholder="email@email.com"
+                />{' '}
+              </div>
+
+              <div className="formGroupMargin">
+                <p> &#128477; Public Key &#x2A;</p>
+                <input
+                  placeholder="Enter your name"
+                  className="formArea__inner__first__input"
+                  type="text"
+                />{' '}
+              </div>
+            </div>
+          </Col>
+          <Col lg={6} className="formArea__inner-second">
+            <div className="formGroupMargin">
+              <p>Redirect Uri &#x2A;</p>
+              <input
+                className="formArea__inner__first__input"
+                type="text"
+                placeholder="http://"
+              />{' '}
+            </div>{' '}
+            <div className="formGroupMargin">
+              <p>Email (optional)</p>
+              <input
+                className="formArea__inner__first__input"
+                type="email"
+              />{' '}
+            </div>
+          </Col>
+        </Row>
       </div>
     </ClientListLayout>
   );
